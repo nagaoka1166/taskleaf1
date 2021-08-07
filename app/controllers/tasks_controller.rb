00e40948e48e7388cc95ad37bc0eeead2538ba72
nagaoka1166 <Ryuu11660408@gmail.com>
2021-08-08 02:11:10 +0900

    rails 現場

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+require "byebug"
+
+class TasksController < ApplicationController
+  def index
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク 「#{@task.name}」を登録しました"
+    else
+      render :new
+    end
+  end
+  def index
+    @tasks = Task.all #複数のタスクだからら
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
